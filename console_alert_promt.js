8dbd558fd5250dc2832eb0f7fde914fd321d5bc1
mustafaeyidayi <71007840+mustafaeyidayi@users.noreply.github.com>
2022-02-14 12:58:06 +0300

    console alert ve promt uygulamaları

---
+++ console_alert_promt.js
@@ -0,0 +1,16 @@
+let myAge=27;
+console.log(myAge);
+console.error("wrong number");
+console.warn("please enter a higher number");
+
+
+//bu ikisi arasına yazacagımız kodun ne kadar süre çalıştığını gösterecek.
+console.time("id");
+console.log("asdasdasdasd");
+console.timeEnd("id");
+console.clear();//konsolu temizledik
+
+// alert("new language");
+
+let sayi1=prompt("enter a number");//kullanıcıdan sayı girişi istedik
+console.log(sayi1);
