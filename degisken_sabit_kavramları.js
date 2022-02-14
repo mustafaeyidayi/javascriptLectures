f7fc619c8de9f453c4bef610d2c2b74ebcd9523b
mustafaeyidayi <71007840+mustafaeyidayi@users.noreply.github.com>
2022-02-14 12:34:29 +0300

    defining variables

---
+++ degisken_sabit_kavramları.js
@@ -0,0 +1,13 @@
+var yasim=27;
+//ES6 ile var kelimesi yerine artık let ve const kullanılıyor
+//noktalı virgül kullanıp kullannmamak bize kalmış ama çeşitli programlama dillerinde kullanıldıgı için kullanılması daha mantıklı
+let tcKimlik=652153;
+// degişken isimlendirmesi yaparken ayrılmış kelimeler ve başlangıçta sayı ile başlamamyacagız
+let sayi1=454545;
+// uzun yazımlı degişkenleri bekirlerken camel case yazım yapmaya özen göster yani kelimelerin ilk halfleri büyük ve bitişik yazım.
+let benimDogumYilim=1994;
+// yorum satırı hariç türkçe karakter ve boşluk kullanmıyoruz
+let sayı2;
+sayi2=50;
+//sabit sayılarda const kullanırız değişmesini istemeyiz çünkü
+const piSayisi=3.14;
