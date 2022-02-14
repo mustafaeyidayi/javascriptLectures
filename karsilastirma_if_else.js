245a43c2b6f909bbf9af674bd3eb3f90022078ea
mustafaeyidayi <71007840+mustafaeyidayi@users.noreply.github.com>
2022-02-14 16:32:23 +0300

    ternary if ve switch case kullanımı

--- karsilastirma_if_else.js
+++ karsilastirma_if_else.js
@@ -28,3 +28,18 @@ if(sorgu){
     //bu kısmı çalıştırırız çünkü ifin condion parantezinin içi true/false analizi yapar.
 
 }
+
+let a=20, b=15, c=0;
+if(a>b){
+    c=a+b;
+}
+else{
+    c=a-b;
+}
+console.log("c degeri= "+c);
+
+//ternary operator of if 
+//a büyük mü be diye bakıyor dogruysa il yazılan öncül yanlışsa ikinci yazılanı baz alıyor.
+
+c=(a>b)?(a+b):(a-b)
+console.log("c degeri= "+c);
