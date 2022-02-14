92a96e2cebe01eecc23872b0b39383fabab041b2
mustafaeyidayi <71007840+mustafaeyidayi@users.noreply.github.com>
2022-02-14 15:50:15 +0300

    if ve else if yapıları

---
+++ karsilastirma_if_else.js
@@ -0,0 +1,30 @@
+//iften sonra normal parantez ve süslü parantez açmayı unutma else if olursa da yine aynı şekilde.
+//else durumu diğer durumlar olmadığında gerçekleşeceginden 
+//bir condition parantezi açmaya gerek yok süslü parantez yeterli.
+//süslü parantezi tek satırda bir kod yazdığımızda kullanmayız.
+
+
+let sayi1=25, sayi2=25;
+if(sayi1<sayi2){
+
+console.log("sayi1 küçüktür sayi2");
+
+}
+else if(sayi1=sayi2){
+   
+ console.log("sayi1 eşittir sayi2");
+
+}
+else{
+
+console.log("sayi1 büyüktür sayi2");
+
+}
+
+let sorgu=true;
+
+if(sorgu){
+
+    //bu kısmı çalıştırırız çünkü ifin condion parantezinin içi true/false analizi yapar.
+
+}
