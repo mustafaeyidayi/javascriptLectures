16daa0d6c9a2b3e8fe827f427780a71bc7d48400
mustafaeyidayi <71007840+mustafaeyidayi@users.noreply.github.com>
2022-02-28 14:02:37 +0300

    installing bulma css framework

--- async/fetch.js
+++ async/fetch.js
@@ -7,16 +7,25 @@ function getText(e){
 
 e.preventDefault();
 
-    fetch('ogrenci.json').then(snc => snc.json())
-        .then(response => {
-            let cikti = '';
-            response.forEach(ogr => {
+    // fetch('ogrenci.json').then(snc => snc.json())
+    //     .then(response => {
+    //         let cikti = '';
+    //         response.forEach(ogr => {
 
-                cikti += `ögrenci adi ${ogr.ad} id ${ogr.id}`
-            })
-            sonucDiv.innerText = cikti;
-        })
-        
+    //             cikti += `ögrenci adi ${ogr.ad} id ${ogr.id}`
+    //         })
+    //         sonucDiv.innerText = cikti;
+    //     })
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(sonuc=>ekranaYazdır(sonuc))
 
     
+}
+function ekranaYazdır(data) {
+    let cikti='';
+    data.forEach(element => {
+        cikti+=`<li>${element.name}</li>`
+    });
+    sonucDiv.innerHTML=cikti;
 }
